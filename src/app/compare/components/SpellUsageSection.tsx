082dd3d1a8aceac7f0f6d916c3a2f3a52519332a
koalth <koalrube@gmail.com>
2024-10-24 18:11:37 -0400

    added graphs

--- src/app/compare/components/SpellUsageSection.tsx
+++ src/app/compare/components/SpellUsageSection.tsx
@@ -4,21 +4,97 @@ import { ComparisonSection } from "./shared/ComparsionSection";
 import { Chart } from "react-google-charts";
 
 const SpellUsageChart = () => {
-  const data = [
-    ["Spell #1", "Spell #2"],
-    [15, 30],
-    [20, 19],
+
+  const spellData = [
+    {
+      name: "Prayer of Mending",
+      player1Casts: 42,
+      player2Casts: 57,
+    },
+    {
+      name: "Flash Heal",
+      player1Casts: 78,
+      player2Casts: 65,
+    },
+    {
+      name: "Renew",
+      player1Casts: 35,
+      player2Casts: 42,
+    },
+    {
+      name: "Circle of Healing",
+      player1Casts: 28,
+      player2Casts: 31,
+    },
+    {
+      name: "Divine Hymn",
+      player1Casts: 2,
+      player2Casts: 3,
+    },
   ];
 
+  const data = [
+    [
+      "Spell",
+      "player1Name",
+      { role: "style" },
+      "player2Name",
+      { role: "style" },
+    ],
+    ...spellData.map((spell) => [
+      spell.name,
+      spell.player1Casts,
+      "#8b5cf6", // Customizable color for player 1
+      spell.player2Casts,
+      "#10b981", // Customizable color for player 2
+    ]),
+  ];
+
+  const options = {
+    titleTextStyle: {
+      fontName: "Inter, system-ui, sans-serif",
+      fontSize: 16,
+      bold: true,
+    },
+    backgroundColor: "transparent",
+    chartArea: {
+      width: "80%",
+      height: "70%",
+    },
+    hAxis: {
+      title: "Spells",
+      titleTextStyle: {
+        fontName: "Inter, system-ui, sans-serif",
+        italic: false,
+      },
+      textStyle: {
+        fontName: "Inter, system-ui, sans-serif",
+      },
+    },
+    vAxis: {
+      title: "Number of Casts",
+      titleTextStyle: {
+        fontName: "Inter, system-ui, sans-serif",
+        italic: false,
+      },
+      textStyle: {
+        fontName: "Inter, system-ui, sans-serif",
+      },
+      minValue: 0,
+    },
+    legend: {
+      position: "top",
+      alignment: "center",
+      textStyle: {
+        fontName: "Inter, system-ui, sans-serif",
+      },
+    },
+    seriesType: "bars",
+    bar: { groupWidth: "80%" },
+  };
+
   return (
-    <Chart
-      chartType="ColumnChart"
-      data={data}
-      options={{
-        title: "Spell Usage",
-      }}
-      legendToggle
-    />
+    <Chart chartType="ColumnChart" data={data} options={options} legendToggle />
   );
 };
 
--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -3,7 +3,46 @@ import { FightSelect } from "./components/FightSelect";
 import { QuickCompare } from "./components/QuickCompare";
 import { SpellUsageSection } from "./components/SpellUsageSection";
 
-export default function Page() {
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const fetchData = async () => {
+  const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  client
+    .query({
+      query: gql`
+        query {
+          characters(page: 2, filter: { name: "rick" }) {
+            info {
+              count
+            }
+            results {
+              name
+            }
+          }
+          location(id: 1) {
+            id
+          }
+          episodesByIds(ids: [1, 2]) {
+            id
+          }
+        }
+      `,
+    })
+    .then((result) => console.log(result));
+};
+
+export default async function Page() {
+  await fetchData();
+
   return (
     <div className="min-h-screen bg-gray-50">
       <main className="mx-auto max-w-7xl px-4 pb-8">
