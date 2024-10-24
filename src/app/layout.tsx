b1d64c7fa120fb37fed4ccad9d56b0334eba2c57
koalth <koalrube@gmail.com>
2024-10-24 10:37:57 -0400

    fixed home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" data-theme="mytheme" className={`${GeistSans.variable}`}>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
+import { Search } from "lucide-react";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
+        <h1 className="bg-gradient-to-r from-secondary to-accent bg-clip-text text-5xl font-extrabold tracking-tight text-transparent sm:text-[5rem]">
           WoW Buddy
         </h1>
-        <div className="flex flex-row">
-          <input
-            type="url"
-            placeholder="Paste your warcraft log url..."
-            className="Input w-full rounded border p-2"
-          />
-          <button type="submit" className="rounded">
+        <div className="flex flex-row gap-5">
+          <label className="input input-bordered flex w-full items-center gap-2">
+            <Search />
+            <input
+              type="text"
+              className="grow"
+              placeholder="Warcraft log url"
+            />
+          </label>
+          <button type="submit" className="btn btn-accent">
             Search
           </button>
         </div>
